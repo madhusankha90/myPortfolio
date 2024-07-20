2a30ab0c610c8985b90fbfc2e8f8b4aa4921e635
madhusankha90 <madhusankha.networking@gmail.com>
2024-07-21 04:14:41 +0530

    update package.json

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // output: 'export',
+  assetPrefix: './',
+  basePath: '/myPortfolio',
 };
 
 export default nextConfig;
